13556f5954c367838f6ae94978487b4b58948030
qmranik <qmranik@gmail.com>
2022-11-10 17:14:26 +0600

    server information added

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://quiet-reaches-09436.herokuapp.com/'
+  uri: 'https://qmranik-social-media-app.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
